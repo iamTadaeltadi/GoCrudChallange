bd262b2e0b0524481c000476de5eb70f7674881b
iamTadaeltadi <tadaelshewaregagebre@icloud.com>
2024-04-26 23:44:33 +0300

    dockerize the api and add testing

--- myapp/main.go
+++ myapp/main.go
@@ -4,10 +4,24 @@ import (
 	"log"
 	"myproject/internal/api/routes"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	log.Println("Starting server on port 8080...")
+	err := godotenv.Load(filepath.Join("..", ".env"))
+	if err != nil {
+		log.Println("Error loading .env file:", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
+
+	log.Printf("Starting server on port %s...\n", port)
 	http.Handle("/", routes.NewRouter())
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
